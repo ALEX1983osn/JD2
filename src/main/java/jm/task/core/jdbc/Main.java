9358d3bbfdd0974eecb1f99b7dbeb793e526391a
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix
    Signed-off-by: pilotAlexey737 <pilotofboeing737@gmail.com>

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Alexey ", "Ovsyannikov", (byte) 40);
+        userDao.saveUser("Maria", "Ovsaynnikova", (byte) 13);
+        userDao.saveUser("Maxim", "Galkin", (byte) 51);
+        userDao.saveUser("Umma", "Turman", (byte) 47);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,48 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+public class UserServiceImpl extends Util implements UserService {
+    private UserDao userDaoJDBC = new UserDaoJDBCImpl();
+
+    @Override
+    public void createUsersTable() throws SQLException {
+        userDaoJDBC.createUsersTable();
+    }
+
+    @Override
+    public void dropUsersTable() throws SQLException {
+        userDaoJDBC.dropUsersTable();
 
     }
 
-    public void dropUsersTable() {
-
+    @Override
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        userDaoJDBC.saveUser(name, lastName, age);
+        System.out.println("User с именем " + name + " добавлен в базу данных");
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    @Override
+    public void removeUserById(long id) throws SQLException {
+        userDaoJDBC.removeUserById(id);
     }
 
-    public void removeUserById(long id) {
-
+    @Override
+    public List<User> getAllUsers() throws SQLException {
+        userDaoJDBC.getAllUsers();
+        return userDaoJDBC.getAllUsers();
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    @Override
+    public void cleanUsersTable() throws SQLException {
+        userDaoJDBC.cleanUsersTable();
     }
 
-    public void cleanUsersTable() {
 
-    }
 }
